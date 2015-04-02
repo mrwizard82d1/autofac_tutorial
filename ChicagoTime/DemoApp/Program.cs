3ba45f3831a46a8522b5bcec6f89a684df46262d
Larry Jones <mrwizard82d1@gmail.com>
2015-04-02 10:04:46 -0500

    Implement `Main()` to write a date.
    `Main()` begins by registering components with the container. It
    later calls `WriteDate()` to actually write the date. The method
    `WriteDate()` creates a lifetime scope. Within that scope it asks the
    container to resolve a concrete implementation of `IDateWriter`. The
    container resolves that by instatiating a `TodayWriter`. To instantiate
    this class requires an implementation of `IOutput`. The container
    resolves that implementation to the concrete `ConsoleOutput` type.

--- ChicagoTime/DemoApp/Program.cs
+++ ChicagoTime/DemoApp/Program.cs
@@ -1,15 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Autofac;
+using Autofac.Core;
 
 namespace DemoApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ConsoleOutput>().As<IOutput>();
+            builder.RegisterType<TodayWriter>().As<IDateWriter>();
+            Container = builder.Build();
+
+            // The WriteDate method is where we'll make use of our
+            // dependency injection. We'll define that in a bit.
+            WriteDate();
+
+            // Ensure the created window **does not** disappear when
+            // run from Visual Studio.
+            Console.WriteLine("Press <ENTER> when finished.");
+            Console.ReadLine();
         }
+
+        public static IContainer Container { get; set; }
+
+        private static void WriteDate()
+        {
+            // Create a lifetime scope (to avoid Autofac holding references
+            // to objects for the lifetime of the container (that is, for
+            // the duration of the entire application).
+            using (var lifetimeScope = Container.BeginLifetimeScope())
+            {
+                // Within this scope, the container resolves the concrete
+                // implementation of IDateWriter. Because it is resolved
+                // within a specific scope, the container itself **does
+                // not** hold onto the created reference throughout the 
+                // durationof the application.
+                var writer = lifetimeScope.Resolve<IDateWriter>();
+
+                writer.WriteDate();
+            }
+        }
+
     }
 }
